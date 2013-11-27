b7825e2edc187fa2c1568a0c6524186c0bbb9d33
Yizhong Liang <liang.yizhong@epfl.ch>
2013-11-27 16:28:36 +0100

    122

--- 122.cc
+++ 122.cc
@@ -1,92 +1,100 @@
+#include <algorithm>
+#include <cstdio>
 #include <cstring>
 #include <iostream>
-#include <sstream>
-#include <string>
+#include <list>
 #include <vector>
 using namespace std;
 
+const int MAX_LENGTH = 5000;  // Must be sufficiently large!
 int n;
 bool can[1000][1000];
-int cycle[1000];
-char line[2500];
+bool used[1000];
+list<int> cycle;
+char line[MAX_LENGTH];
+int conn[1000];  // The direct connection in the cycle.
 
-void expand() {
-  vector<bool> go(n, false);
-  cycle[0] = 0;
-  for (int i = 1; i < n; i++) {
-    cycle[i] = 0;
-    for (int j = 1; j < n; j++) {
-      if (can[cycle[i - 1]][j] && !go[j]) {
-        go[j] = true;
-        cycle[i] = j;
-        break;
-      }
-    }
-    if (cycle[i] == 0) {
-      for (int j = 1; j < n; j++) {
-        if (!go[j]) {
-          go[j] = true;
-          cycle[i] = j;
-          break;
-        }
-      }
-    }
-  }
-}
-
-void swap(int& i, int &j) {
-  int temp = i;
-  i = j;
-  j = temp;
-}
-
-void reverse(int l, int r) {
-  while (l != r) {
-    swap(cycle[l], cycle[r]);
-    l = (l + 1) % n;
-    if (l == r) break;
-    r = (r + n - 1) % n;
-  }
+void update_connection(int i) {
+  for (int j = 0; j < n; j++)
+    if (can[i][j]) conn[j] = i;
 }
 
 int main() {
-  // Input.
-  cin >> n;
-  string line;
-  getline(cin, line);
+  // Input. Use C I/O, otherwise TLE.
+  fgets(line, MAX_LENGTH, stdin);
+  n = atoi(line);
   for (int i = 0; i < n; i++) {
-    getline(cin, line);
-    istringstream iss(line);
-    int ami;
-    while (iss >> ami) can[i][ami - 1] = can[ami - 1][i] = true;
+    fgets(line, MAX_LENGTH, stdin);
+    char* token = strtok(line, " ");
+    while (token != NULL) {
+      int ami = atoi(token);
+      can[i][ami - 1] = can[ami - 1][i] = true;
+      token = strtok(NULL, " ");
+    }
   }
-  // Palmer's algorithm.
-  expand();
-  bool gap;
-  do {
-    gap = false;
+  // Main algorithm.
+  for (int i = 0; i < n; i++) conn[i] = -1;
+  cycle.push_back(0);
+  used[0] = true;
+  update_connection(0);
+  while (cycle.size() < n) {
+    bool found = false;
     for (int i = 0; i < n; i++) {
-      int next_i = (i + 1) % n;
-      if (can[cycle[i]][cycle[next_i]]) continue;
-      gap = true;
-      for (int j = 0; j < n; j++) {
-        int next_j = (j + 1) % n;
-        if (j == i || next_j == i || j == next_i || next_j == next_i) continue;
-        if (can[cycle[i]][cycle[j]] && can[cycle[next_i]][cycle[next_j]]) {
-          reverse(next_i, j);
-          break;
-        }
+      if (used[i]) continue;
+      if (can[i][cycle.front()]) {
+        used[i] = found = true;
+        cycle.push_front(i);
+        update_connection(i);
+        break;
+      } else if (can[i][cycle.back()]) {
+        used[i] = found = true;
+        cycle.push_back(i);
+        update_connection(i);
+        break;
       }
     }
-  } while (gap);
-  // Output.
-  int pos = 0;
-  while (cycle[pos] != 0) pos++;
-  cout << cycle[pos] + 1;
-  for (int i = 0; i < n; i++) {
-    pos = (pos + 1) % n;
-    cout << ' ' << cycle[pos] + 1;
+    if (!found) break;
   }
-  cout << endl;
+  while (true) {
+    // Is the path a circle?
+    if (can[cycle.front()][cycle.back()]) {
+      if (cycle.size() == n) break;
+      // Extend the circle. O(n).
+      int i = 0;
+      while (used[i] || conn[i] == -1) i++;
+      used[i] = true;
+      list<int>::iterator it = cycle.begin();
+      while (*it != conn[i]) it++;
+      rotate(cycle.begin(), it, cycle.end());
+      cycle.push_front(i);
+      update_connection(i);
+    } else {
+      // Rearrange the path to be a cycle. O(n).
+      // There exists c[k-1] and c[k] s.t. c[0] connects c[k] and c[len - 1]
+      // connects c[k - 1].
+      list<int>::iterator it1 = cycle.begin();
+      list<int>::iterator it2 = cycle.begin();
+      it2++;
+      while (it2 != cycle.end()) {
+        if (can[cycle.front()][*it2] && can[*it1][cycle.back()]) {
+          reverse(it2, cycle.end());
+          break;
+        }
+        it1++;
+        it2++;
+      }
+    }
+  }
+  // Output.
+  for (list<int>::iterator it = cycle.begin(); it != cycle.end(); it++) {
+    if (*it == 0) {
+      rotate(cycle.begin(), it, cycle.end());
+      break;
+    }
+  }
+  for (list<int>::iterator it = cycle.begin(); it != cycle.end(); it++) {
+    cout << *it + 1 << ' ';
+  }
+  cout << cycle.front() + 1 << endl;
   return 0;
 }
