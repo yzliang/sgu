59d6de0c52ba4df95d75f21c77700421b8d4cd29
Yizhong Liang <liang.yizhong@epfl.ch>
2013-11-29 11:55:47 +0100

    125

--- 125.cc
+++ 125.cc
@@ -58,15 +58,6 @@ int main() {
   for (int i = 0; i <= n + 1; i++)
     for (int j = 0; j <= n + 1; j++)
       a[i][j] = -1;
-  // Fill the known place.
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n; j++) {
-      if (b[i][j] == 0)
-        a[i][j] = 9;
-      else if (b[i][j] == 4)
-        a[i][j] = 0;
-    }
-  }
   if (!search(1, 1)) cout << "NO SOLUTION" << endl;
   return 0;
 }
