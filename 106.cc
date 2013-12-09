6d9bce9e7dc796aeff337aa29aa02ff1a3d757de
Yizhong Liang <liang.yizhong@epfl.ch>
2013-12-09 18:07:14 +0100

    106 corrected

--- 106.cc
+++ 106.cc
@@ -1,70 +1,74 @@
-#include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <stdint.h>
 using namespace std;
 
-int main() {
-  long a, b, c;
-  long x1, x2, y1, y2;
-  cin >> a >> b >> c >> x1 >> x2 >> y1 >> y2;
-
-  if (x1 > x2 || y1 > y2) {
-    cout << 0 << endl;
-    return 0;
-  }
-
-  if (a == 0 && b == 0) {
-    cout << (c == 0 ? (x2 - x1 + 1) * (y2 - y1 + 1) : 0) << endl;
-    return 0;
-  }
-
-  long min_x, max_x, min_y, max_y;
-  if (a != 0) {
-    double v1 = static_cast<double>(-b * y1 - c) / a;
-    double v2 = static_cast<double>(-b * y2 - c) / a;
-    if (v1 <= v2) {
-      min_x = ceil(v1);
-      max_x = floor(v2);
-    } else {
-      min_x = ceil(v2);
-      max_x = floor(v1);
-    }
-  }
-
-  if (b != 0) {
-    double v1 = static_cast<double>(-a * x1 - c) / b;
-    double v2 = static_cast<double>(-a * x2 - c) / b;
-    if (v1 <= v2) {
-      min_y = ceil(v1);
-      max_y = floor(v2);
-    } else {
-      min_y = ceil(v2);
-      max_y = floor(v1);
-    }
-  }
-
-  min_x = max(min_x, x1);
-  max_x = min(max_x, x2);
-  min_y = max(min_y, y1);
-  max_y = min(max_y, y2);
-
-  // Horizontal.
-  if (a == 0) {
-    cout << (min_y == max_y ? x2 - x1 + 1 : 0) << endl;
-    return 0;
-  }
-
-  // Vertical.
+int64_t extended_euclid(int64_t a, int64_t b, int64_t& x, int64_t& y) {
   if (b == 0) {
-    cout << (min_x == max_x ? y2 - y1 + 1 : 0) << endl;
-    return 0;
+    x = 1;
+    y = 0;
+    return a;
+  } else {
+    int64_t x1, y1;
+    int64_t d = extended_euclid(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - (a / b) * y1;
+    return d;
   }
+}
 
-  if (min_x > max_x || min_y > max_y) {
-    cout << 0 << endl;
-    return 0;
+int64_t compute(int64_t a, int64_t b, int64_t c, int64_t x1, int64_t x2,
+                int64_t y1, int64_t y2) {
+  if (y1 > y2 || x1 > x2) return 0;
+  if (a == 0 && b == 0) {
+    if (c == 0)
+      return (int64_t) (y2 - y1 + 1) * (x2 - x1 + 1);
+    else
+      return 0;
+  } else if (a == 0) {
+    if (c % b == 0 && c / b >= y1 && c / b <= y2)
+      return x2 - x1 + 1;
+    else
+      return 0;
+  } else if (b == 0) {
+    if (c % a == 0 && c / a >= x1 && c / a <= x2)
+      return y2 - y1 + 1;
+    else
+      return 0;
   }
+  // Unification: a * x + b * y = -c, a > 0
+  c = -c;
+  if (a < 0) {
+    a = -a;
+    b = -b;
+    c = -c;
+  }
+  int64_t x, y;
+  int64_t d = extended_euclid(a, abs(b), x, y);
+  if (b < 0) y = -y;
+  // ax + by = d
+  if (c % d != 0) return 0;
+  // solution space:
+  //   x = {(c/d)x0 + (b/d)*k}, x1<=x<=x2
+  //   y = {(c/d)y0 - (a/d)*k}, y1<=y<=y2
+  double upper = (double) (x2 - c / d * x) / (b / d);
+  double lower = (double) (x1 - c / d * x) / (b / d);
+  if (b < 0) {
+    double temp = upper;
+    upper = lower;
+    lower = temp;
+  }
+  upper = min(upper, (double) (c / d * y - y1) / (a / d));
+  lower = max(lower, (double) (c / d * y - y2) / (a / d));
+  upper = floor(upper);
+  lower = ceil(lower);
+  return upper >= lower ? upper - lower + 1 : 0;
+}
 
-  // a * x = -c (mod b)
+int main() {
+  int64_t a, b, c;
+  int64_t x1, x2, y1, y2;
+  cin >> a >> b >> c >> x1 >> x2 >> y1 >> y2;
+  cout << compute(a, b, c, x1, x2, y1, y2) << endl;
   return 0;
 }
